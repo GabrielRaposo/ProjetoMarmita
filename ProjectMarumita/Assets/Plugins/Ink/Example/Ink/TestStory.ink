1c1adc3099ed21e87b0c5992cf3dfbd361f93453
GabrielRaposo <gabriel.s.raposo@gmail.com>
2018-03-30 11:20:16 -0300

    Adição dos principais plugins do projeto
    - Ink, TextTyper e TextMesh Pro
    - Base da tela de narração

---
+++ ProjectMarumita/Assets/Plugins/Ink/Example/Ink/TestStory.ink
@@ -0,0 +1,24 @@
+//EXTERNAL change_background(ID)
+
+--> BGChangeTest
+
+=== BGChangeTest
+[MC] Finalmente a aula acabou..
+[MC] Bora lá comer!!
+Decidi ir pra copa comer. Foi então que na minha <i>inocência</i>, percebi que estava, quem diria, sem <color="red">marmita!</color="red"> Nunca senti um desespero tão grande na minha vida! Eu estava com tanta fome que poderia desmaiar a qualquer momento!!
+[MC] E ainda por cima é quinta feira...
+[MC] Dia de peixe no bandeijão..!!
+[MC] O que que eu faço?
+* Arranjar dinheiro pra comprar um quentinha
+    Sim sim, a gente sempre pode dar uns jeitos!
+    ** Roubar carteira do amiguinho que está dormindo na aula
+        Cristianismo -5!!!!!!
+    ** Trabalhar
+        Lol não
+    ** Fazer nada a esperar que as coisas funcionem sozinhas
+        FUNCIONOU!!
+    -> DONE
+* Gastar todo o dinheiro em gacha
+    -> DONE
+Okay!
+--> END
---
+++ ProjectMarumita/Assets/Scripts/InkManager.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using TMPro;
+using Ink.Runtime;
+using RedBlueGames.Tools.TextTyper;
+
+public class InkManager : MonoBehaviour {
+
+    public TextAsset inkJSONAsset;
+    public TextMeshProUGUI narrationBox, dialogBox;
+    public TextTyper narrationTyper;
+    public Button buttonPrefab;
+    public RectTransform buttonsCanvas;
+
+    private Story story;
+    private bool textIsRunning;
+
+    //typewrite stuff
+    [Range(1f, 10f)] public float textSpeed = 5;
+    [Range(1f, 10f)] public float commaDelayMultiplier = 5;
+    [Range(1f, 10f)] public float ponctuationDelayMultiplier = 5;
+
+    AudioSource typeSound;
+
+    enum State { Narration, Dialog }
+
+    private void Awake() {
+        typeSound = GetComponent<AudioSource>();
+
+        StartStory();
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            if (narrationTyper.IsSkippable()) narrationTyper.Skip();
+        }
+    }
+
+    private void StartStory() {
+        story = new Story(inkJSONAsset.text);
+        SetExternalFunction();
+
+        CallStorytelling();
+    }
+
+    private void SetExternalFunction() {
+        story.BindExternalFunction("change_background", (int ID) => ChangeBackground(ID));
+    }
+
+    private void ClearTexts() {
+        if (narrationBox) narrationBox.text = string.Empty;
+        if (dialogBox) dialogBox.text = string.Empty;
+    }
+
+    private void CallStorytelling() {
+        ClearTexts();
+        ClearButtons();
+
+        string text = string.Empty;
+        while (story.canContinue) {
+            string section = story.Continue();
+            //checa se é dialogo
+            text += section;
+        }
+        narrationTyper.TypeText(text, 1f / (textSpeed*10));
+        narrationTyper.PrintCompleted.RemoveAllListeners();
+        narrationTyper.PrintCompleted.AddListener(() => StartCoroutine(SetText()));
+        narrationTyper.CharacterPrinted.AddListener((string str) => PlayTypeSound());
+    }
+
+    private void PlayTypeSound() {
+        if(!typeSound.isPlaying) typeSound.Play();
+    }
+
+    private void CallButtons() {
+        if (story.currentChoices.Count > 0) {
+            for (int i = 0; i < story.currentChoices.Count; i++)
+            {
+                Choice choice = story.currentChoices[i];
+                Button button = CreateChoiceView(choice.text.Trim());
+                button.onClick.AddListener(delegate
+                {
+                    OnClickChoiceButton(choice);
+                });
+            }
+        } else {
+            log("call End of Story.");
+            Button choice = CreateChoiceView("End of story.\nRestart?");
+            choice.onClick.AddListener(delegate { StartStory(); });
+        }
+    }
+
+    IEnumerator SetText() {
+        if (story.canContinue) {
+            yield return new WaitForSeconds(1); 
+            CallStorytelling();
+        } else {
+            CallButtons();
+        }
+    }
+
+    Button CreateChoiceView (string text) {
+		Button choice = Instantiate (buttonPrefab) as Button;
+		choice.transform.SetParent (buttonsCanvas.transform, false);
+
+        TextMeshProUGUI choiceText = choice.GetComponentInChildren<TextMeshProUGUI>();
+		choiceText.text = text;
+
+		HorizontalLayoutGroup layoutGroup = choice.GetComponent <HorizontalLayoutGroup> ();
+		layoutGroup.childForceExpandHeight = false;
+
+		return choice;
+	}
+
+    void OnClickChoiceButton (Choice choice) {
+		story.ChooseChoiceIndex (choice.index);
+        CallStorytelling();
+	}
+
+    void ClearButtons() {
+        int childCount = buttonsCanvas.transform.childCount;
+        for (int i = childCount - 1; i >= 0; --i) {
+            GameObject.Destroy(buttonsCanvas.transform.GetChild(i).gameObject);
+        }
+    }
+
+    void SetCharacter(int ID) {
+
+    }
+
+    void ChangeBackground(int ID) {
+        Debug.Log("ID: " + ID);
+    }
+
+    void log(string str) { Debug.Log(str); }
+}
